cb3de677e32ffaca1b4e4dfdb64b78bc59233389
ANDRE LUIS DE FREITAS NASCIMENTO <a95752490@rn.senac.local>
2026-07-23 12:42:47 -0300

    correção dos valores no index

--- src/views/index.ts
+++ src/views/index.ts
@@ -3,32 +3,32 @@ import { Produto } from "./models/product_model";
 
 
 // Criando o produto com as informações reais
-const celular = new Produto(
+const cama = new Produto(
     "1", 
-    "iPhone 15", 
-    "Celular da Apple", 
-    5000, 
-    "Smartphones", 
-    "http://imagem.com/iphone.jpg"
+    "Cama Box Casal",
+    " 1,38 m Suede Prince",
+    300,
+    "quarto",
+    "imagem da cama"
 );
 
-console.log(celular.preco); // Usa o GETTER -> 5000
+console.log(cama.preco); // Usa o GETTER -> 5000
 
 // Alterando o preço (Usa o SETTER)
-celular.preco = 4500; 
-console.log(celular.preco); // 4500
+cama.preco = 300; 
+console.log(cama.preco); // 4500
 
 // Tentando colocar um preço inválido (O SETTER vai barrar)
 try {
-    celular.preco = -100;
+    cama.preco = -100;
 } catch (error: any) {
     console.error(error.message); // Imprime: "O preço não pode ser negativo."
-    console.log(celular.preco);   // Continua sendo 4500, o valor foi protegido
+    console.log(cama.preco);   // Continua sendo 4500, o valor foi protegido
 }
 
 // Tentando colocar um nome inválido
 try {
-    celular.nome = "AB"; // Menos de 3 caracteres
+    cama.nome = "AB"; // Menos de 3 caracteres
 } catch (error: any) {
     console.error(error.message); // Imprime: "O nome do produto deve ter pelo menos 3 caracteres."
 }
